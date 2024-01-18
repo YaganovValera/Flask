d974b3ce4f541e44e1b13c7a5db1f35ecb14602f
Valera <99064072+YaganovValera@users.noreply.github.com>
2024-01-19 00:00:17 +0400

    Update models.py

--- lesson_3/models.py
+++ lesson_3/models.py
@@ -1,7 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+
 
 db = SQLAlchemy()
 
@@ -12,19 +10,3 @@ class User(db.Model):
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
-
-class RegistrationForm(FlaskForm):
-    first_name = StringField('Имя', validators=[DataRequired()])
-    last_name = StringField('Фамилия', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6),
-                                                   EqualTo('confirm_password',
-                                                   message='Пароли должны совпадать')])
-    confirm_password = PasswordField('Повторите пароль', validators=[DataRequired()])
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
